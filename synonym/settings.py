6184c35685e1a7cf54a6b90c71f35990ae9f03b7
rhit-ravishs <92815732+rhit-ravishs@users.noreply.github.com>
2022-02-20 21:31:24 -0500

    Update settings.py

--- synonym/settings.py
+++ synonym/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+django_heroku.settings(locals())
